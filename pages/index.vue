12870859e01498e0f702e85e8dd24bf52e5ea34b
LuscaLima <lusca.lima@outlook.com>
2024-05-18 13:32:40 -0300

    chore: Defines template and style for player one mark selection page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <div class="w-full h-screen bg-cyan-950"></div>
+  <div class="flex justify-center items-center w-full h-screen bg-sky-950">
+    <div class="flex gap-8">
+      <NuxtLink to="/simple" custom v-slot="{ navigate }">
+        <AppButton @click="navigate">Play simple</AppButton>
+      </NuxtLink>
+    </div>
+  </div>
 </template>
---
+++ pages/simple/index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import AppX from "~/components/AppX.vue";
+import AppO from "~/components/AppO.vue";
+</script>
+
+<template>
+  <div class="flex justify-center items-center w-full h-screen p-4 bg-sky-950">
+    <div class="flex flex-col gap-8 w-full md:w-96">
+      <div class="p-4 bg-sky-900 shadow-block shadow-sky-darker rounded-xl">
+        <div class="mb-4 uppercase text-center text-sm text-gray-50">
+          <strong> pick player 1's mark </strong>
+        </div>
+        <div>
+          <AppSelectButton
+            :options="[
+              { label: AppX, value: 'x' },
+              { label: AppO, value: 'o' },
+            ]"
+            default-value="x"
+          />
+        </div>
+        <div class="mt-4 uppercase text-center text-xs text-gray-300">
+          <small>remember: X goes first</small>
+        </div>
+      </div>
+      <footer class="flex flex-col gap-4">
+        <AppButton>new game (vs cpu)</AppButton>
+        <AppButton variant="secondary">new game (vs player)</AppButton>
+      </footer>
+    </div>
+  </div>
+</template>
