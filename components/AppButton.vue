cdd592283dc52661abbac34e9b6876457ddc0876
LuscaLima <lusca.lima@outlook.com>
2024-05-19 12:02:21 -0300

    feat(button): Define size prop

--- components/AppButton.vue
+++ components/AppButton.vue
@@ -1,26 +1,38 @@
 <script setup lang="ts">
 const props = withDefaults(
   defineProps<{
-    variant?: "primary" | "secondary" | "danger";
+    variant?: "primary" | "secondary" | "neutral";
+    size?: "small" | "medium" | "large";
   }>(),
-  { variant: "primary" }
+  {
+    variant: "primary",
+    size: "medium",
+  }
 );
 
-const classes = computed(() => {
+const variants = computed(() => {
   return {
     primary:
       "bg-amber-500 text-sky-950 shadow-amber-600 hover:bg-amber-600 hover:shadow-amber-700",
     secondary:
       "bg-cyan-500 text-sky-950 shadow-cyan-600 hover:bg-cyan-600 hover:shadow-cyan-700",
-    danger: "bg-red-500 text-white",
+    neutral:
+      "bg-gray-300 text-sky-950 shadow-gray-400 hover:bg-gray-400 hover:shadow-gray-500",
   }[props.variant];
+});
+const sizes = computed(() => {
+  return {
+    small: "h-9 px-9 rounded-lg",
+    medium: "h-12 px-12 rounded-xl",
+    large: "h-[3.75rem] px-[3.75rem] rounded-2xl",
+  }[props.size];
 });
 </script>
 
 <template>
   <button
-    class="h-12 px-12 rounded-xl uppercase font-bold shadow-block select-none transition-all duration-300"
-    :class="classes"
+    class="uppercase font-bold shadow-block select-none transition-all duration-300"
+    :class="[variants, sizes]"
   >
     <slot />
   </button>
